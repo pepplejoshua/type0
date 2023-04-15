373daf3c53b1df19cdfcbca9020b721f84d1583f
Joshua Pepple <pepplejoshuatams@gmail.com>
2023-04-15 12:44:10 -0600

    Added symbol and spaces parser functions

--- Main.hs
+++ Main.hs
@@ -1,8 +1,20 @@
 module Main where
 
 import System.Environment (getArgs)
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+symbol :: Parser Char
+symbol = oneOf "!$%&|*+-/:<=>?@^_~#"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+readExpr :: String -> String
+readExpr src = case parse (spaces >> symbol) "type0" src of
+  Left err -> "No match: " ++ show err
+  Right val -> "Found value"
 
 main :: IO ()
 main = do
   args <- getArgs
-  putStrLn ("Hello, " ++ head args)
+  putStrLn (readExpr (head args))
