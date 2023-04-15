96aba417fe4bb3fec2e29967cfb3f00b8e0729ba
Joshua Pepple <pepplejoshuatams@gmail.com>
2023-04-15 13:31:04 -0600

    Defined atom parser

--- Main.hs
+++ Main.hs
@@ -3,11 +3,36 @@ module Main where
 import System.Environment (getArgs)
 import Text.ParserCombinators.Parsec hiding (spaces)
 
+data Type0Val
+  = Atom String
+  | List [Type0Val]
+  | DotList [Type0Val] Type0Val
+  | Number Integer
+  | Str String
+  | Boolean Bool
+
 symbol :: Parser Char
 symbol = oneOf "!$%&|*+-/:<=>?@^_~#"
 
 spaces :: Parser ()
 spaces = skipMany1 space
+
+parseString :: Parser Type0Val
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ Str x
+
+parseAtom :: Parser Type0Val
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = first : rest
+  return $ case atom of
+    "#t" -> Boolean True
+    "$f" -> Boolean False
+    _ -> Atom atom
 
 readExpr :: String -> String
 readExpr src = case parse (spaces >> symbol) "type0" src of
