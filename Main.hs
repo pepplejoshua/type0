c137fcc8ef34e6f85638ddbaef4aaaad0b1632d0
Joshua Pepple <pepplejoshuatams@gmail.com>
2023-04-15 16:43:07 -0600

    Impld R5RS Strs, Chars and Hex/Oct numbers

--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Numeric (readDec, readHex, readOct)
 import System.Environment (getArgs)
 import Text.ParserCombinators.Parsec hiding (spaces)
 
@@ -9,6 +10,7 @@ data Type0Val
   | DotList [Type0Val] Type0Val
   | Number Integer
   | Str String
+  | Charac Char
   | Boolean Bool
 
 symbol :: Parser Char
@@ -27,17 +29,17 @@ parseString = do
 escaped :: Parser Char
 escaped = do
   char '\\'
-  char '"'
-  return '"'
-
-escapedSpaces :: Parser Char
-escapedSpaces = do
-  oneOf "\r\n\t\\"
+  res <- try (oneOf "\\\"") <|> oneOf "rnt"
+  return $ case res of
+    'r' -> '\r'
+    'n' -> '\n'
+    't' -> '\t'
+    _ -> res
 
 parseStringR5RS :: Parser Type0Val
 parseStringR5RS = do
   char '"'
-  x <- many $ escapedSpaces <|> escaped <|> noneOf "\""
+  x <- many $ escaped <|> noneOf "\""
   char '"'
   return $ Str x
 
@@ -54,25 +56,78 @@ parseAtom = do
 parseNumber :: Parser Type0Val
 parseNumber = Number . read <$> many1 digit
 
-parseNumber2 :: Parser Type0Val
-parseNumber2 = do
-  digits <- many1 digit
-  let num = read digits :: Integer
+parseNumberStd :: Parser Type0Val
+parseNumberStd = do
+  numTag <- try (string "#x") <|> try (string "#o") <|> return ""
+  let base
+        | numTag == "#x" = 16
+        | numTag == "#o" = 8
+        | otherwise = 10
+  numStr <-
+    if null numTag
+      then many1 digit
+      else many1 (oneOf "0123456789abcdefABCDEF")
+  let num = case readDecOrHex base numStr of
+        [(n, "")] -> n
+        _ -> error "Invalid number"
   return $ Number num
 
-parseNumber3 :: Parser Type0Val
-parseNumber3 = many1 digit >>= \digits -> return $ Number (read digits :: Integer)
+readDecOrHex :: Int -> String -> [(Integer, String)]
+readDecOrHex base str
+  | base == 8 = readOct str
+  | base == 10 = readDec str
+  | base == 16 = readHex str
+  | otherwise = error "Invalid base"
+
+parseChar :: Parser Type0Val
+parseChar = do
+  _ <- char '#'
+  _ <- char '\\'
+  c <- anyChar
+  c <- case c of
+    't' -> return '\t'
+    'n' -> return '\n'
+    'r' -> return '\r'
+    '"' -> return '\"'
+    '\'' -> return '\''
+    '\\' -> return '\\'
+    'x' -> do
+      hexDigits <- count 2 hexDigit
+      case readHex hexDigits of
+        [(n, "")] -> return $ toEnum n
+        _ -> fail "Invalid hex escape"
+    'u' -> do
+      hexDigits <- count 4 hexDigit
+      case readHex hexDigits of
+        [(n, "")] -> return $ toEnum n
+        _ -> fail "Invalid Unicode escape"
+    _ -> return c
+  return $ Charac c
 
 parseExpr :: Parser Type0Val
 parseExpr =
-  parseAtom
+  parseNumberStd
+    <|> parseChar
     <|> parseStringR5RS
-    <|> parseNumber3
+    <|> parseAtom
 
 readExpr :: String -> String
 readExpr src = case parse (spaces >> parseExpr) "type0" src of
   Left err -> "No match: " ++ show err
-  Right val -> "Found value"
+  Right val -> case val of
+    Atom s -> do
+      "Atom " ++ s
+    Number n -> do
+      "Number " ++ show n
+    Str ss -> do
+      "Str " ++ ss
+    Boolean True -> do
+      "Boolean true"
+    Boolean False -> do
+      "Boolean false"
+    Charac ch -> do
+      "Character " ++ [ch]
+    _ -> "Matched Input!"
 
 main :: IO ()
 main = do
