bc5a06befaf8e96d9e4b901d91ab2e583f899d03
Joshua Pepple <pepplejoshuatams@gmail.com>
2023-04-15 15:07:11 -0600

    Impl'd r5rs compliant string parser + others

--- Main.hs
+++ Main.hs
@@ -24,6 +24,23 @@ parseString = do
   char '"'
   return $ Str x
 
+escaped :: Parser Char
+escaped = do
+  char '\\'
+  char '"'
+  return '"'
+
+escapedSpaces :: Parser Char
+escapedSpaces = do
+  oneOf "\r\n\t\\"
+
+parseStringR5RS :: Parser Type0Val
+parseStringR5RS = do
+  char '"'
+  x <- many $ escapedSpaces <|> escaped <|> noneOf "\""
+  char '"'
+  return $ Str x
+
 parseAtom :: Parser Type0Val
 parseAtom = do
   first <- letter <|> symbol
@@ -34,8 +51,26 @@ parseAtom = do
     "$f" -> Boolean False
     _ -> Atom atom
 
+parseNumber :: Parser Type0Val
+parseNumber = Number . read <$> many1 digit
+
+parseNumber2 :: Parser Type0Val
+parseNumber2 = do
+  digits <- many1 digit
+  let num = read digits :: Integer
+  return $ Number num
+
+parseNumber3 :: Parser Type0Val
+parseNumber3 = many1 digit >>= \digits -> return $ Number (read digits :: Integer)
+
+parseExpr :: Parser Type0Val
+parseExpr =
+  parseAtom
+    <|> parseStringR5RS
+    <|> parseNumber3
+
 readExpr :: String -> String
-readExpr src = case parse (spaces >> symbol) "type0" src of
+readExpr src = case parse (spaces >> parseExpr) "type0" src of
   Left err -> "No match: " ++ show err
   Right val -> "Found value"
 
